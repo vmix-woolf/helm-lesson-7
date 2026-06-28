865fca5048ac2f3b97b9fb43ed3ec8d890d798da
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2026-06-28 20:19:19 +0300

    Enable remote Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,12 +1,9 @@
-# Remote backend will be enabled after the S3 bucket and DynamoDB table are created.
-# The backend resources are defined in modules/s3-backend.
-#
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-lesson-7-mykhailov-viacheslav-20260628"
-#     key            = "lesson-7/terraform.tfstate"
-#     region         = "us-west-2"
-#     dynamodb_table = "terraform-locks-lesson-7"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket       = "terraform-state-lesson-7-mykhailov-viacheslav-20260628"
+    key          = "lesson-7/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
